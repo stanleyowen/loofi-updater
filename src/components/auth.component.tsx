bbea39846def4a2cbc4dc0c6339b9b6ac1dd21e4
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-05-05 10:16:04 +0700

    Initial Commit

---
+++ src/components/auth.component.tsx
@@ -0,0 +1,147 @@
+import React, { useState, useEffect } from 'react';
+import { initializeApp } from 'firebase/app';
+import { generateToken } from '../lib/functions.component';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { Alert, Slide, Button, Snackbar, SlideProps } from '@mui/material';
+
+type TransitionProps = Omit<SlideProps, 'direction'>;
+
+const About = ({ config, handleCredential }: any) => {
+    initializeApp(config);
+    const whiteListedEmails: string[] | undefined =
+        process.env.REACT_APP_WHITELISTED_EMAIL?.split(', ');
+    const [status, setStatus] = useState<{
+        isError: boolean;
+        message:
+            | null
+            | 'Invalid Credentials'
+            | 'Something Went Wrong. Please Try Again Later.'
+            | 'No Whitelisted Emails are Found.';
+    }>({
+        isError: false,
+        message: 'Invalid Credentials',
+    });
+    const [transition, setTransition] = useState<
+        React.ComponentType<TransitionProps> | undefined
+    >(undefined);
+
+    useEffect(() => {
+        for (let i = 0; i < 79; i++) {
+            const div = document.createElement('div');
+            div.style.opacity = `${Math.random() * (0.075 - 0.025) + 0.025}`;
+            document.querySelector('.backdrop-overlay')?.appendChild(div);
+        }
+    }, []);
+
+    return (
+        <div>
+            <div className="backdrop-overlay"></div>
+            <div className="backdrop">
+                <div className="acrylic-material"></div>
+                <div className="backdrop-image" id="backdrop-image"></div>
+            </div>
+
+            <div className="bg-white container p-10 rounded-corner">
+                <h3 className="center-align mb-10">Welcome Back!</h3>
+                <Button
+                    variant="outlined"
+                    className="mt-10 w-100"
+                    onClick={() => {
+                        signInWithPopup(getAuth(), new GoogleAuthProvider())
+                            .then((result) => {
+                                function Transition(props: TransitionProps) {
+                                    return (
+                                        <Slide {...props} direction="right" />
+                                    );
+                                }
+                                setTransition(() => Transition);
+                                if (result) {
+                                    if (whiteListedEmails)
+                                        whiteListedEmails.forEach(
+                                            (email, index) => {
+                                                if (
+                                                    result.user.email === email
+                                                ) {
+                                                    generateToken(
+                                                        result.user.email
+                                                    );
+                                                    handleCredential({
+                                                        id: 'isLoading',
+                                                        value: false,
+                                                    });
+                                                } else if (
+                                                    index ===
+                                                    whiteListedEmails.length - 1
+                                                ) {
+                                                    setStatus({
+                                                        isError: true,
+                                                        message:
+                                                            'Invalid Credentials',
+                                                    });
+                                                    handleCredential({
+                                                        id: 'isLoading',
+                                                        value: false,
+                                                    });
+                                                }
+                                            }
+                                        );
+                                    else {
+                                        setStatus({
+                                            isError: true,
+                                            message:
+                                                'No Whitelisted Emails are Found.',
+                                        });
+                                        setTimeout(
+                                            () =>
+                                                setStatus({
+                                                    isError: false,
+                                                    message: null,
+                                                }),
+                                            5000
+                                        );
+                                    }
+                                } else {
+                                    setStatus({
+                                        isError: true,
+                                        message:
+                                            'Something Went Wrong. Please Try Again Later.',
+                                    });
+                                    setTimeout(
+                                        () =>
+                                            setStatus({
+                                                isError: false,
+                                                message: null,
+                                            }),
+                                        5000
+                                    );
+                                }
+                            })
+                            .catch(() => {
+                                setStatus({
+                                    isError: true,
+                                    message:
+                                        'Something Went Wrong. Please Try Again Later.',
+                                });
+                                setTimeout(
+                                    () =>
+                                        setStatus({
+                                            isError: false,
+                                            message: null,
+                                        }),
+                                    5000
+                                );
+                            });
+                    }}
+                >
+                    Sign in with Google
+                </Button>
+            </div>
+
+            <Snackbar open={status.isError} TransitionComponent={transition}>
+                <Alert severity="error">{status.message}</Alert>
+            </Snackbar>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/base.component.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import Logs from './logs.component';
+import Home from './home.component';
+import Music from './music.component';
+import Settings from './settings.component';
+
+// eslint-disable-next-line
+const BaseLayout = ({
+    song,
+    properties,
+    songData,
+    handleSong,
+    HOST_DOMAIN,
+    rawSongData,
+    handleChange,
+}: any) => {
+    return (
+        <div className="base">
+            {properties.activeTab === 'home' ? (
+                <Home
+                    properties={properties}
+                    handleSong={handleSong}
+                    song={song}
+                    songData={songData}
+                    HOST_DOMAIN={HOST_DOMAIN}
+                    handleChange={handleChange}
+                />
+            ) : properties.activeTab === 'music' ? (
+                <Music
+                    properties={properties}
+                    handleSong={handleSong}
+                    songData={songData}
+                    HOST_DOMAIN={HOST_DOMAIN}
+                    rawSongData={rawSongData}
+                />
+            ) : properties.activeTab === 'logs' ? (
+                <Logs properties={properties} HOST_DOMAIN={HOST_DOMAIN} />
+            ) : (
+                <Settings />
+            )}
+        </div>
+    );
+};
+
+export default BaseLayout;
